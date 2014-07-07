1abf304e4501448aa136eb14f81b17b12f8ee97d
gorhill <rhill@raymondhill.net>
2014-07-07 18:53:14 -0400

    scripts to generate store-sepcific packages

---
+++ make-clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# This script assumes a linux environment
+
+echo "*** uBlock: Cleaning."
+rm -R dist/ublock/*
+echo "*** uBlock: Cleaned."
--- make-opera.sh
+++ make-opera.sh
@@ -3,7 +3,7 @@
 # This script assumes a linux environment
 
 echo "*** uBlock: Creating Opera web store package"
-./make-package.sh
+./make-chrome.sh
 rm -r dist/ublock/_locales/ru
 rm -r dist/ublock/_locales/zh_CN
 echo "*** uBlock: Opera package done."
