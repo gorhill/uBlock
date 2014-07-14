b3b3ff3049a56e7632de6713512431303a01dd4d
gorhill <rhill@raymondhill.net>
2014-07-14 13:07:04 -0400

    now ru can ship

--- make-opera.sh
+++ make-opera.sh
@@ -6,7 +6,6 @@ echo "*** uBlock: Creating Opera web store package"
 ./make-chrome.sh
 rm -r dist/ublock/_locales/da
 rm -r dist/ublock/_locales/pl
-rm -r dist/ublock/_locales/ru
 rm -r dist/ublock/_locales/uk
 rm -r dist/ublock/_locales/zh_CN
 echo "*** uBlock: Opera package done."
