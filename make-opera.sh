acd1560358da1eb8d4831761f1311961d265bf57
gorhill <rhill@raymondhill.net>
2014-07-14 13:34:26 -0400

    cant ship Turkish translation to Opera yet, they require the description

--- make-opera.sh
+++ make-opera.sh
@@ -6,6 +6,7 @@ echo "*** uBlock: Creating Opera web store package"
 ./make-chrome.sh
 rm -r dist/ublock/_locales/da
 rm -r dist/ublock/_locales/pl
+rm -r dist/ublock/_locales/tr
 rm -r dist/ublock/_locales/uk
 rm -r dist/ublock/_locales/zh_CN
 echo "*** uBlock: Opera package done."
