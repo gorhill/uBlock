2b3b29c10d2bfc0f26559839b5d5d4b5c8c3a817
gorhill <rhill@raymondhill.net>
2014-07-17 11:38:51 -0400

    description in zh_CN now available

--- make-opera.sh
+++ make-opera.sh
@@ -7,6 +7,4 @@ echo "*** uBlock: Creating Opera web store package"
 rm -r dist/ublock/_locales/da
 rm -r dist/ublock/_locales/pl
 rm -r dist/ublock/_locales/tr
-rm -r dist/ublock/_locales/uk
-rm -r dist/ublock/_locales/zh_CN
 echo "*** uBlock: Opera package done."
