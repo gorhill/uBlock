1577ce471ddbba2a4cc34958739d9e26b967734b
gorhill <rhill@raymondhill.net>
2014-07-21 11:51:37 -0400

    updated as per latest available description files

--- make-opera.sh
+++ make-opera.sh
@@ -4,7 +4,8 @@
 
 echo "*** uBlock: Creating Opera web store package"
 ./make-chrome.sh
-rm -r dist/ublock/_locales/da
+rm -r dist/ublock/_locales/hu
+rm -r dist/ublock/_locales/ja
 rm -r dist/ublock/_locales/pl
 rm -r dist/ublock/_locales/tr
 echo "*** uBlock: Opera package done."
