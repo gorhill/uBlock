59d29bd93f5985006e782767e897a1e29d67f073
gorhill <rhill@raymondhill.net>
2014-08-18 10:44:43 -0400

    hebrew and polish description now available

--- make-opera.sh
+++ make-opera.sh
@@ -5,9 +5,7 @@
 echo "*** uBlock: Creating Opera web store package"
 ./make-chrome.sh
 rm -r dist/ublock/_locales/es
-rm -r dist/ublock/_locales/he
 rm -r dist/ublock/_locales/hu
 rm -r dist/ublock/_locales/ja
-rm -r dist/ublock/_locales/pl
 rm -r dist/ublock/_locales/tr
 echo "*** uBlock: Opera package done."
