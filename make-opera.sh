6ad8e80bfac471c571a2d6a5043d506f8fef1247
gorhill <rhill@raymondhill.net>
2014-09-08 12:49:03 -0400

    Opera requries full description to be available

--- make-opera.sh
+++ make-opera.sh
@@ -7,4 +7,5 @@ echo "*** uBlock: Creating Opera web store package"
 rm -r dist/ublock/_locales/es
 rm -r dist/ublock/_locales/hu
 rm -r dist/ublock/_locales/ja
+rm -r dist/ublock/_locales/vi
 echo "*** uBlock: Opera package done."
