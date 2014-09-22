4ccaa17594939253cad219b4cb22ff6488ca2b83
gorhill <rhill@raymondhill.net>
2014-09-22 11:32:46 -0400

    needed to force re-parsing of filters following #245

--- js/background.js
+++ js/background.js
@@ -89,7 +89,7 @@ return {
     firstUpdateAfter: 5 * oneMinute,
     nextUpdateAfter: 7 * oneHour,
 
-    selfieMagic: 'tgfnfcmhdqls',
+    selfieMagic: 'imktayytaizj',
     selfieAfter: 7 * oneMinute,
 
     pageStores: {},
