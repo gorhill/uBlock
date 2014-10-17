2cdb3f68bf323d620e73e41f6c605a025da23950
Deathamns <deathamns@gmail.com>
2014-10-17 17:04:36 +0200

    Added Hungarian description

--- make-opera.sh
+++ make-opera.sh
@@ -6,7 +6,6 @@ echo "*** uBlock: Creating Opera web store package"
 ./make-chrome.sh
 rm -r dist/ublock/_locales/el
 rm -r dist/ublock/_locales/es
-rm -r dist/ublock/_locales/hu
 rm -r dist/ublock/_locales/ja
 rm -r dist/ublock/_locales/vi
 echo "*** uBlock: Opera package done."
