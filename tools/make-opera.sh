4527252a039427fc890000868efb9dab378f7fbb
gorhill <rhill@raymondhill.net>
2014-12-13 17:11:22 -0500

    fixed Opera script

--- tools/make-opera.sh
+++ tools/make-opera.sh
@@ -2,9 +2,24 @@
 #
 # This script assumes a linux environment
 
-echo "*** uBlock: Creating Opera web store package"
-./make-chrome.sh
+echo "*** uBlock.opera: Creating web store package"
+echo "*** uBlock.opera: Copying files"
+
+DES=dist/build/uBlock.opera
+rm -r $DES
+mkdir -p $DES
+
+cp -R assets $DES/
+rm $DES/assets/*.sh
+cp -R src/css $DES/
+cp -R src/img $DES/
+cp -R src/js $DES/
+cp -R src/lib $DES/
+cp -R src/_locales $DES/
+cp src/*.html $DES/
+cp platform/chromium/*.js $DES/js/
+cp platform/chromium/manifest.json $DES/
+
 rm -r dist/ublock/_locales/el
-rm -r dist/ublock/_locales/ja
 rm -r dist/ublock/_locales/vi
-echo "*** uBlock: Opera package done."
+echo "*** uBlock.opera: Package done."
