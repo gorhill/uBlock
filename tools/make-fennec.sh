70488274b1d1a1ddfbdafd5c5ad65a2a55871dac
gorhill <rhill@raymondhill.net>
2015-02-15 23:05:50 -0500

    as per comment, this should work well enough

--- tools/make-fennec.sh
+++ tools/make-fennec.sh
@@ -19,10 +19,10 @@ cp src/*.html $DES/
 mv $DES/img/icon_128.png $DES/icon.png
 cp platform/firefox/vapi-*.js $DES/js/
 cp platform/firefox/bootstrap.js $DES/
-cp platform/firefox/frameScript.js $DES/
+cp platform/firefox/frame*.js $DES/
 cp platform/firefox/chrome.manifest $DES/
 cp platform/firefox/install.rdf $DES/
-cp platform/fennec/frameModule.js $DES/
+cp platform/fennec/vapi-*.js $DES/
 cp LICENSE.txt $DES/
 
 echo "*** uBlock.fennec: Generating meta..."
