caea8c5497abd8603236e7ef97dd4d9241ab5c3b
AlexVallat <github@alex.vallat.name>
2015-04-02 09:35:52 +0100

    Prevent resetting of block count statistics in the more normal case of onLocationChange occurring before document-element-inserted

--- platform/firefox/frameModule.js
+++ platform/firefox/frameModule.js
@@ -294,10 +294,12 @@ const contentObserver = {
             let doc = e.target;
             doc.removeEventListener(e.type, docReady, true);
 
+            // It is possible, in some cases (#1140) for document-element-inserted to occur *before* nsIWebProgressListener.onLocationChange, so ensure that the URL is correct before continuing
             let messageManager = doc.docShell.getInterface(Ci.nsIContentFrameMessageManager);
 
             messageManager.sendSyncMessage(locationChangedMessageName, {
                 url: loc.href,
+                noRefresh: true, // If the URL is the same, then don't refresh it so that if this occurs after onLocationChange, no the block count isn't reset
             });
 
             lss(this.contentBaseURI + 'contentscript-end.js', sandbox);
