f396e95a859c43ad90d6b73d3bcddaa8a6822118
gorhill <rhill@raymondhill.net>
2015-04-07 15:13:03 -0400

    updated repo-only 3rd-party assets

--- assets/update-3p-mirror.sh
+++ assets/update-3p-mirror.sh
@@ -6,42 +6,25 @@ TEMPFILE=/tmp/httpsb-asset
 
 echo "*** uBlock: updating remote assets..."
 
-THIRDPARTY_REMOTEURLS=(
-    'http://mirror1.malwaredomains.com/files/immortal_domains.txt'
-    'http://mirror1.malwaredomains.com/files/justdomains'
-    'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext'
-    'http://www.malwaredomainlist.com/hostslist/hosts.txt'
-    'http://hosts-file.net/.%5Cad_servers.txt'
-    'http://someonewhocares.org/hosts/hosts'
-    'http://winhelp2002.mvps.org/hosts.txt'
-    'https://publicsuffix.org/list/effective_tld_names.dat'
-    )
+declare -A assets
+assets=(
+    ['thirdparties/mirror1.malwaredomains.com/files/immortal_domains.txt']='http://mirror1.malwaredomains.com/files/immortal_domains.txt'
+    ['thirdparties/mirror1.malwaredomains.com/files/justdomains']='http://mirror1.malwaredomains.com/files/justdomains'
+    ['thirdparties/publicsuffix.org/list/effective_tld_names.dat']='https://publicsuffix.org/list/effective_tld_names.dat'
+)
 
-THIRDPARTY_LOCALURLS=(
-    'thirdparties/mirror1.malwaredomains.com/files/immortal_domains.txt'
-    'thirdparties/mirror1.malwaredomains.com/files/justdomains'
-    'thirdparties/pgl.yoyo.org/as/serverlist'
-    'thirdparties/www.malwaredomainlist.com/hostslist/hosts.txt'
-    'thirdparties/hosts-file.net/ad-servers'
-    'thirdparties/someonewhocares.org/hosts/hosts'
-    'thirdparties/winhelp2002.mvps.org/hosts.txt'
-    'thirdparties/publicsuffix.org/list/effective_tld_names.dat'
-    )
-
-ENTRY_INDEX=0
-for THIRDPARTY_REMOTEURL in ${THIRDPARTY_REMOTEURLS[@]}; do
-    THIRDPARTY_LOCALURL=${THIRDPARTY_LOCALURLS[ENTRY_INDEX]}
-    echo "*** Downloading" $THIRDPARTY_REMOTEURL
-    if wget -q -T 30 -O $TEMPFILE -- $THIRDPARTY_REMOTEURL; then
+for i in "${!assets[@]}"; do
+    localURL="$i"
+    remoteURL="${assets[$i]}"
+    echo "*** Downloading ${remoteURL}"
+    if wget -q -T 30 -O $TEMPFILE -- $remoteURL; then
         if [ -s $TEMPFILE ]; then
-            if ! cmp -s $TEMPFILE $THIRDPARTY_LOCALURL; then
-                echo "    New version found: $THIRDPARTY_LOCALURL"
+            if ! cmp -s $TEMPFILE $localURL; then
+                echo "    New version found: ${localURL}"
                 if [ "$1" != "dry" ]; then
-                    mv $TEMPFILE $THIRDPARTY_LOCALURL
+                    mv $TEMPFILE $localURL
                 fi
             fi
         fi
     fi
-    let ENTRY_INDEX+=1
 done
-
