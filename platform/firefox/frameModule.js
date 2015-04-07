ad2bc8df74038f8f47c28c0309130f310c4d78b4
gorhill <rhill@raymondhill.net>
2015-04-07 16:27:11 -0400

    removed spurious lines of code

--- platform/firefox/frameModule.js
+++ platform/firefox/frameModule.js
@@ -168,9 +168,6 @@ const contentObserver = {
             type: type,
             url: location.spec
         };
-        if ( type === 7 ) {
-            details.attrSrc = context.frameElement.getAttribute('src');
-        }
 
         if ( typeof messageManager.sendRpcMessage === 'function' ) {
             // https://bugzil.la/1092216
--- platform/firefox/vapi-background.js
+++ platform/firefox/vapi-background.js
@@ -1311,9 +1311,6 @@ vAPI.net.registerListeners = function() {
             type: details.type,
             url: details.url
         };
-        if ( details.attrSrc !== undefined ) {
-            lastRequest[0].attrSrc = details.attrSrc;
-        }
     };
 
     vAPI.messaging.globalMessageManager.addMessageListener(
