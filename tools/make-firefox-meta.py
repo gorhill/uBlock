237af0dd42701b86ef50eccddc1c2403aa0be8d6
Chris <chris@chrismatic.io>
2015-04-08 16:17:06 -0600

    Fix Firefox link to repo

--- tools/make-firefox-meta.py
+++ tools/make-firefox-meta.py
@@ -73,7 +73,7 @@ chromium_manifest = pj(proj_dir, 'platform', 'chromium', 'manifest.json')
 with open(chromium_manifest, encoding='utf-8') as m:
     manifest = json.load(m)
 
-manifest['homepage'] = 'https://github.com/gorhill/uBlock'
+manifest['homepage'] = 'https://github.com/chrisaljoudi/uBlock'
 manifest['description'] = descriptions['en']
 del descriptions['en']
 manifest['localized'] = []
