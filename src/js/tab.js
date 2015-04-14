fda75755b8b8417d3a51c5f5f1dfd1d304942021
Chris <chris@chrismatic.io>
2015-04-13 19:18:12 -0600

    No. of blocked badge should update with popups

--- src/js/tab.js
+++ src/js/tab.js
@@ -471,6 +471,9 @@ vAPI.tabs.onPopup = function(details) {
     if ( pageStore ) {
         pageStore.logRequest(context, result);
     }
+    if ( µb.userSettings.showIconBadge ) {
+        µb.updateBadgeAsync(details.openerTabId);
+    }
 
     // Not blocked
     if ( µb.isAllowResult(result) ) {
