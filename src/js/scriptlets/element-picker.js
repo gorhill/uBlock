aeeaa4d5b32ed4418b7157c6d8b3011d377b214e
gorhill <rhill@raymondhill.net>
2015-06-17 13:49:43 -0400

    code review

--- src/js/scriptlets/element-picker.js
+++ src/js/scriptlets/element-picker.js
@@ -952,6 +952,9 @@ var startPicker = function(details) {
         showDialog({ modifier: true });
         return;
     }
+
+    // A target was specified, but it wasn't found: abort.
+    stopPicker();
 };
 
 /******************************************************************************/
