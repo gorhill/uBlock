bc11167941ed173a7af5e5d3875d64e31e25947d
chrisaljoudi <chris@chrismatic.io>
2015-08-14 21:42:54 -0600

    Fixes #1518

--- platform/safari/vapi-client.js
+++ platform/safari/vapi-client.js
@@ -239,7 +239,7 @@
         }
         document.addEventListener(vAPI.sessionId, function(e) {
             if(shouldBlockDetailedRequest(e.detail)) {
-                e.detail.url = false;
+                document.body.setAttribute("blocked", "true");
             }
         }, true);
         var tmpJS = document.createElement("script");
@@ -251,8 +251,9 @@ var block = function(u, t) {" +
 e.initCustomEvent('" + vAPI.sessionId + "', false, false, {url: u, type: t});"
 : "var e = new CustomEvent('" + vAPI.sessionId + "', {bubbles: false, detail: {url: u, type: t}});"
 ) +
-"document.dispatchEvent(e);\
-return e.detail.url === false;\
+"document.body.setAttribute('blocked', '');\
+document.dispatchEvent(e);\
+return !!document.body.getAttribute('blocked');\
 },\
 wo = open,\
 xo = XMLHttpRequest.prototype.open,\
