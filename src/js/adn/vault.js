c2e67f961c69ed8785b91f713badf65a430d32f1
Cyrus S <yuyhero@gmail.com>
2016-02-24 16:40:39 +0800

    Fix #13

--- src/js/adn/vault.js
+++ src/js/adn/vault.js
@@ -653,7 +653,7 @@ function itemPosition($ele) {
 
   var off = $ele.offset(), // relative to container
     cx = $(window).width() / 2,
-    cy = $(window).height() / 2.
+    cy = $(window).height() / 2,
     iw = $ele.attr('data-width') || 80,
     ih = $ele.attr('data-height') || 40;
 
