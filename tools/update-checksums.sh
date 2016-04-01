9b1850adf06c1f63df9918146c467ec2f7222b0a
Daniel Howe <daniel@rednoise.org>
2016-04-02 00:04:28 +0800

    fixed checksum update for osx, and updated checksums for new adnauseam.txt

--- tools/update-checksums.sh
+++ tools/update-checksums.sh
@@ -6,7 +6,11 @@ echo "*** uBlock: generating checksums.txt file..."
 truncate -s 0 assets/checksums.txt
 LIST="$(find assets/ublock assets/thirdparties -type f)"
 for ENTRY in $LIST; do
-    echo `md5sum $ENTRY` >> assets/checksums.txt
+    # for osx
+    echo `md5sum -q $ENTRY` $ENTRY >> assets/checksums.txt
+
+    # for linux
+    #echo `md5sum $ENTRY` >> assets/checksums.txt
 done
 
 echo "*** uBlock: checksums updated."
