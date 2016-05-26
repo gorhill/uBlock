07addb14cf4e5b0bbae8aad025e3a3f6391611b6
Daniel Howe <daniel@rednoise.org>
2016-05-26 16:04:50 +0800

    split messages.json into two files

--- tools/make-chromium.sh
+++ tools/make-chromium.sh
@@ -2,6 +2,8 @@
 #
 # This script assumes a linux environment
 
+hash jq 2>/dev/null || { echo; echo >&2 "Error: this script requires jq (https://stedolan.github.io/jq/), but it's not installed"; exit 1; }
+
 echo "*** adnauseam.chromium: Creating web store package"
 echo "*** adnauseam.chromium: Copying files"
 
@@ -11,12 +13,26 @@ mkdir -p $DES
 
 ./tools/make-assets.sh $DES
 
+# handle concatenating local files
+
+FILES=src/_locales/**/adnauseam.json
+for adnfile in $FILES
+do
+  messages="${adnfile/adnauseam/messages}"
+  out="${messages/src/$DES}"
+  dir=`dirname $out`
+  mkdir -p $dir && touch $out
+  echo Writing $out
+  jq -s '.[0] * .[1]' $messages $adnfile > $out
+done
+
+
 cp -R src/css               $DES/
 cp -R src/img               $DES/
 cp -R src/js                $DES/
 cp -R src/lib               $DES/
-cp -R src/_locales          $DES/
-cp -R $DES/_locales/nb      $DES/_locales/no
+#cp -R src/_locales          $DES/
+#cp -R $DES/_locales/nb      $DES/_locales/no
 cp src/*.html               $DES/
 cp platform/chromium/*.js   $DES/js/
 cp -R platform/chromium/img $DES/
