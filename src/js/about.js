dac0677dd748bcab8f3a355f2202b78660fddc04
Daniel Howe <daniel@rednoise.org>
2016-06-24 15:29:24 +0800

    styling of about tab in settings

--- src/js/about.js
+++ src/js/about.js
@@ -7,15 +7,12 @@ uDom.onLoad(function () {
   'use strict';
 
   var onAppDataReady = function (appData) {
-    uDom('#aboutNameVer').text(appData.name +
-      ' v' + appData.version + ' (built on uBlock)');
+    uDom('#aboutNameVer').text(appData.name +' v' + appData.version);
+    uDom('#builtOn').text('built on'); //TODO: intl
   };
 
   vAPI.messaging.send('dashboard', {
     what: 'getAppData'
   }, onAppDataReady);
-
-  //if (document.location.href.endsWith('fr=1')) // show first-run content
-    //uDom('#first-run-content').removeClass('hide');
 
 });
