88f7c068ee9314d78cc0bfb792957dffba57d75f
Leon Eckert <eckertleon@gmail.com>
2016-09-07 23:27:05 -0400

    hide/show box on firstrun.html

--- src/js/adn/firstrun.js
+++ src/js/adn/firstrun.js
@@ -30,20 +30,44 @@
     changeUserSettings(name, value);
   };
 
+  function hideOrClickTrue () {
+    var showBox = false;
+    uDom('[data-setting-type="bool"]').forEach(function (uNode) {
+      var name = uNode.attr('data-setting-name');
+      if((name === 'hidingAds' || name === 'clickingAds') && uNode.prop('checked')){
+        showBox = true;
+      }
+    });
+    return showBox;
+  }
+
+  function toggleDNTExceptionBox(bool){
+    var elem = uDom('#dnt-exception')["nodes"][0].parentElement;
+    if(hideOrClickTrue()){
+      elem.style.display = "block";
+    }else{
+      elem.style.display = "none";
+    }
+  }
+
   /******************************************************************************/
 
   // TODO: use data-* to declare simple settings
 
   var onUserSettingsReceived = function (details) {
 
-    uDom('[data-setting-type="bool"]').forEach(function (uNode) {
 
+    uDom('[data-setting-type="bool"]').forEach(function (uNode) {
       uNode.prop('checked', details[uNode.attr('data-setting-name')] === true)
         .on('change', function () {
+
           changeUserSettings(
             this.getAttribute('data-setting-name'),
             this.checked
           );
+          
+          toggleDNTExceptionBox();
+
         });
     });
 
@@ -54,9 +78,12 @@
 
     uDom('#confirm-close').on('click', function (e) {
       e.preventDefault();
-      // handle #371 here
+      // handle #371 here 
+      // is this function not only called on submit, why #371 here?
       window.open(location, '_self').close();
     });
+
+    toggleDNTExceptionBox();
   };
 
   /******************************************************************************/
