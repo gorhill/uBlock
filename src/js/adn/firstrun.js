e9bc5e44bad26db055fadbaa21723763b62cdee2
dhowe <github.utensil@mailhero.io>
2016-09-22 20:59:59 +0800

    refactoring for code style and proper use of uDom

--- src/js/adn/firstrun.js
+++ src/js/adn/firstrun.js
@@ -44,6 +44,7 @@
   };
 
   var onInputChanged = function (ev) {
+
     var input = ev.target;
     var name = this.getAttribute('data-setting-name');
     var value = input.value;
@@ -54,47 +55,48 @@
   };
 
   function switchValue(name) {
+
     return uDom('[data-setting-name=' + name + ']').prop('checked');
   }
 
   function hideOrClick() {
+
     return switchValue('hidingAds') || switchValue('clickingAds');
   }
 
-  function changeDNTexceptions(bool){
+  function changeDNTexceptions(bool) {
+
     changeUserSettings("disableClickingForDNT", bool);
     changeUserSettings("disableHidingForDNT", bool);
   }
 
   function toggleDNTException(bool) {
-    var dntInput = uDom('#dnt-exception')["nodes"][0];
-    var dntInputWrapper = dntInput.parentElement;
-    if (hideOrClick()) {
-      dntInputWrapper.style.display = "block";
-      // this runs once only:
-      if(!dntRespectAppeared){
-        changeDNTexceptions(true);
-        dntInput.checked = true;
-        dntRespectAppeared = true;
-      }
-    } else {
-      dntInputWrapper.style.display = "none";
+
+    var dntInput = uDom('#dnt-exception');
+
+    if (!dntRespectAppeared) { // runs once only
+
+      changeDNTexceptions(true);
+      dntInput.prop('checked', true);
+      dntRespectAppeared = true;
     }
+
+    dntInput.parent().css('display', hideOrClick() ? 'block' : 'none');
   }
 
   /******************************************************************************/
 
   // TODO: use data-* to declare simple settings
-
   var onUserSettingsReceived = function (details) {
 
     uDom('[data-setting-type="bool"]').forEach(function (uNode) {
+
       uNode.prop('checked', details[uNode.attr('data-setting-name')] === true)
         .on('change', function () {
 
-          if(this.getAttribute('data-setting-name') === "respectDNT"){
+          if (this.getAttribute('data-setting-name') === "respectDNT") {
             changeDNTexceptions(this.checked);
-          }else{
+          } else {
             changeUserSettings(
               this.getAttribute('data-setting-name'),
               this.checked
@@ -102,11 +104,11 @@
           }
 
           if (!hideOrClick()) {
+
             changeDNTexceptions(false);
           }
 
           toggleDNTException();
-
         });
     });
 
@@ -116,14 +118,15 @@
     });
 
     uDom('#confirm-close').on('click', function (e) {
+
       e.preventDefault();
+
       // handles #371
       window.open(location, '_self').close();
     });
 
     toggleDNTException();
   };
-
   /******************************************************************************/
 
   uDom.onLoad(function () {
