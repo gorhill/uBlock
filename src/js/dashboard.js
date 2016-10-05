de9de07adc3d1bd50f0f19a26527aba5a6cbb4e8
dhowe <github.utensil@mailhero.io>
2016-10-05 21:31:05 +0800

    fix to #470

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -45,17 +45,14 @@
     }
 
     var tabButton = uDom('[href="#' + pane + '"]');
-    if (!tabButton || tabButton.hasClass('selected')) {
-      return;
-    }
+    if (!tabButton) return;
+
     uDom('.tabButton.selected').toggleClass('selected', false);
     uDom('iframe').attr('src', pane);
+
     tabButton.toggleClass('selected', true);
 
-    if (notifications && notifications.length){
-      renderNotifications(notifications);  
-    }
-
+    notifications && renderNotifications(notifications);
   };
 
   var onTabClickHandler = function (e) {
@@ -84,22 +81,24 @@
     uDom('#ad-list').css('height', '350px');
   };
 
-  
+
   vAPI.messaging.addChannelListener('adnauseam', function (request) {
 
-    // console.log("dashboard.js::GOT BROADCAST", request);
+    // console.log("dashboard.js::BROADCAST", request);
 
     switch (request.what) {
-    
+
 
     case 'notifications':
+
       // see #488
-      renderNotifications(request.notifications);
+      //renderNotifications(request.notifications);
+      loadDashboardPanel(request.notifications);
       // resizeFrame();
       break;
     }
   });
-  
+
 
   /******************************************************************************/
 
@@ -113,7 +112,7 @@
         'adnauseam', {
           what: 'getNotifications'
         }, loadDashboardPanel);
-    
+
   });
 
   /******************************************************************************/
