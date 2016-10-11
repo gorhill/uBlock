02b227291937aee1daa6475dcf8a96af95d73fb3
dhowe <github.utensil@mailhero.io>
2016-10-11 22:33:25 +0800

    removed stray comma

--- src/js/adn/core.js
+++ src/js/adn/core.js
@@ -45,7 +45,7 @@
     'CHN: CJX\'s Annoyance List‎', 'Spam404', 'Anti-Adblock Killer | Reek‎',
     'Fanboy’s Social Blocking List', 'Malware domains (long-lived)‎',
     'Adblock Warning Removal List', 'Malware filter list by Disconnect',
-    'Basic tracking list by Disconnect', 
+    'Basic tracking list by Disconnect'
   ];
 
   // targets on these domains are never internal (may need to be regexs)
