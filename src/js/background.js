d4155bf9e0cb1504c5b8be8ee48b6465b8c5bb64
gorhill <rhill@raymondhill.net>
2016-12-25 17:00:24 -0500

    new revision for dev build + force lists to be recompiled

--- src/js/background.js
+++ src/js/background.js
@@ -108,8 +108,8 @@ return {
 
     // read-only
     systemSettings: {
-        compiledMagic: 'lbmqiweqbvha',
-        selfieMagic: 'mhirtyetynnf'
+        compiledMagic: 'xhjvmgkamffc',
+        selfieMagic: 'xhjvmgkamffc'
     },
 
     restoreBackupSettings: {
