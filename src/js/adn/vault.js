7ee7ff7c207fc0fe1575adbd70fb9f35c6ee045d
cqx931 <cqx931@gmail.com>
2017-03-17 19:19:10 +0800

    hide showing number if show all

--- src/js/adn/vault.js
+++ src/js/adn/vault.js
@@ -92,7 +92,7 @@
 
   var renderAds = function (json) {
 
-    console.log('renderAds: ', json);
+    // console.log('renderAds: ', json);
     gAds = json.data; // store
     addInterfaceHandlers();
     createSlider(true);
@@ -551,12 +551,16 @@
   }
 
   function computeStats(adsets) {
-
+    console.log("computeStats");
     var numVisits = numVisited(gAds);
     $('.since').text(sinceTime(adsets));
     $('#clicked').text(numVisits);
-    $('#detected').text(numFound(adsets));
     $('#total').text(numTotal());
+    $('#detected').text(numFound(adsets));
+    if(numTotal() != numFound(adsets))
+      $('.showing').show();
+    else
+      $('.showing').hide();
     setCost(numVisits);
   }
 
