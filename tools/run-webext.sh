c18943fe945e385c5501d421475d98a004fb31bd
dhowe <daniel@rednoise.org>
2017-09-15 19:47:53 -0400

    added option to specify firefox binary

--- tools/run-webext.sh
+++ tools/run-webext.sh
@@ -6,6 +6,7 @@ tools/make-webext.sh
 
 pushd dist/build/adnauseam.webext
 
-web-ext run -v --no-reload --firefox-profile="${1:-default}"
+#web-ext run -v --no-reload --firefox-profile="${1:-default}"
+web-ext run -v --bc --no-reload --firefox-profile="${1:-default}" --firefox="${2:-firefox}"
 
 popd
