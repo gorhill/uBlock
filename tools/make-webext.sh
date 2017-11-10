fd5f63d850c94f813c909ab83d09dd68a3a1bf57
gorhill <rhill@raymondhill.net>
2017-11-10 11:36:14 -0500

    fix #3196

--- tools/make-webext.sh
+++ tools/make-webext.sh
@@ -29,8 +29,6 @@ cp platform/webext/polyfill.js          $DES/js/
 cp platform/webext/vapi-webrequest.js   $DES/js/
 cp platform/webext/vapi-cachestorage.js $DES/js/
 cp platform/webext/vapi-usercss.js      $DES/js/
-rm $DES/options_ui.html
-rm $DES/js/options_ui.js
 
 echo "*** uBlock0.webext: concatenating content scripts"
 cat $DES/js/vapi-usercss.js > /tmp/contentscript.js
@@ -38,6 +36,11 @@ echo >> /tmp/contentscript.js
 grep -v "^'use strict';$" $DES/js/contentscript.js >> /tmp/contentscript.js
 mv /tmp/contentscript.js $DES/js/contentscript.js
 rm $DES/js/vapi-usercss.js
+
+# Webext-specific
+rm $DES/img/icon_128.png
+rm $DES/options_ui.html
+rm $DES/js/options_ui.js
 
 echo "*** uBlock0.webext: Generating meta..."
 python tools/make-webext-meta.py $DES/
