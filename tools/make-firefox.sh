81b0c8f8d573ab66d47ea29f3427aff11d220456
Raymond Hill <rhill@raymondhill.net>
2018-04-28 08:22:19 -0400

    remove stuff not needed to build firefox-legacy

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -2,9 +2,9 @@
 #
 # This script assumes a linux environment
 
-echo "*** uBlock0.firefox: Copying files"
+echo "*** uBlock0.firefox-legacy: Copying files"
 
-DES=dist/build/uBlock0.firefox
+DES=dist/build/uBlock0.firefox-legacy
 rm -rf $DES
 mkdir -p $DES
 
@@ -21,8 +21,6 @@ mv    $DES/img/icon_128.png             $DES/icon.png
 cp    platform/firefox/css/*            $DES/css/
 cp    platform/firefox/polyfill.js      $DES/js/
 cp    platform/firefox/vapi-*.js        $DES/js/
-cp    platform/chromium/vapi-usercss.real.js $DES/js/
-cp    platform/webext/vapi-usercss.js   $DES/js/
 cp    platform/firefox/bootstrap.js     $DES/
 cp    platform/firefox/processScript.js $DES/
 cp    platform/firefox/frame*.js        $DES/
@@ -32,7 +30,7 @@ cp    platform/firefox/install.rdf      $DES/
 cp    platform/firefox/*.xul            $DES/
 cp    LICENSE.txt                       $DES/
 
-echo "*** uBlock0.firefox: concatenating content scripts"
+echo "*** uBlock0.firefox-legacy: concatenating content scripts"
 cat $DES/js/vapi-usercss.js > /tmp/contentscript.js
 echo >> /tmp/contentscript.js
 grep -v "^'use strict';$" $DES/js/vapi-usercss.real.js >> /tmp/contentscript.js
@@ -42,15 +40,15 @@ mv /tmp/contentscript.js $DES/js/contentscript.js
 rm $DES/js/vapi-usercss.js
 rm $DES/js/vapi-usercss.real.js
 
-echo "*** uBlock0.firefox: Generating meta..."
+echo "*** uBlock0.firefox-legacy: Generating meta..."
 python tools/make-firefox-meta.py $DES/
 
 if [ "$1" = all ]; then
     set +v
-    echo "*** uBlock0.firefox: Creating package..."
+    echo "*** uBlock0.firefox-legacy: Creating package..."
     pushd $DES/ > /dev/null
-    zip ../uBlock0.firefox.xpi -qr *
+    zip ../uBlock0.firefox-legacy.xpi -qr *
     popd > /dev/null
 fi
 
-echo "*** uBlock0.firefox: Package done."
+echo "*** uBlock0.firefox-legacy: Package done."
