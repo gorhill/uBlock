aff363d5edcb77c3a2ad5dba9db1ec8b45580a1b
Alex Silva <ialexsilva@users.noreply.github.com>
2018-09-09 13:19:12 -0300

    update copyright year

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
 
     uBlock Origin - a browser extension to block requests.
-    Copyright (C) 2014-2016 Raymond Hill
+    Copyright (C) 2014-2018 Raymond Hill
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
