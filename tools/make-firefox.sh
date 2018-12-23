a038c8658f109ae5a7e1b1c1d95f6ff2cd07c77f
Raymond Hill <rhill@raymondhill.net>
2018-12-23 18:19:16 -0500

    Attempt to fix build script for Firefox

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -5,7 +5,8 @@
 echo "*** uBlock0.firefox: Creating web store package"
 echo "*** uBlock0.firefox: Copying files"
 
-DES=dist/build/uBlock0.firefox
+BLDIR=dist/build
+DES="$BLDIR"/uBlock0.firefox
 rm -rf $DES
 mkdir -p $DES
 
@@ -56,8 +57,9 @@ if [ "$1" = all ]; then
 elif [ -n "$1" ]; then
     echo "*** uBlock0.firefox: Creating versioned package..."
     pushd $DES > /dev/null
-    zip ../$(basename $DES).xpi -qr * -O ../uBlock0_"$1".firefox.xpi
+    zip ../$(basename $DES).xpi -qr *
     popd > /dev/null
+    mv "$BLDIR"/uBlock0.firefox.xpi "$BLDIR"/uBlock0_"$1".firefox.xpi
 fi
 
 echo "*** uBlock0.firefox: Package done."
