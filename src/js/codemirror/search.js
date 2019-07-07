9d1913a16e911c2580637fbae7a76d5d918039b5
Raymond Hill <rhill@raymondhill.net>
2019-07-07 06:29:14 -0400

    Eat backslashes only for common control characters
    Related issue:
    - https://github.com/uBlockOrigin/uBlock-issues/issues/658

--- src/js/codemirror/search.js
+++ src/js/codemirror/search.js
@@ -160,11 +160,14 @@
         );
     }
 
+    // https://github.com/uBlockOrigin/uBlock-issues/issues/658
+    //   Modified to backslash-escape ONLY widely-used control characters.
     function parseString(string) {
         return string.replace(/\\(.)/g, function(_, ch) {
             if (ch === "n") return "\n";
             if (ch === "r") return "\r";
-            return ch;
+            if (ch === 't') return '\t';
+            return _;
         });
     }
 
