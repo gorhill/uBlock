1df73fc7e94f6731d0b463ad7fb0b0d4616291b4
cqx931 <cqx931@gmail.com>
2019-07-15 13:24:39 -0400

    fix run firefox script

--- tools/run-firefox.sh
+++ tools/run-firefox.sh
@@ -4,7 +4,7 @@ set -e
 
 tools/make-firefox.sh
 
-pushd dist/build/adnauseam.webext
+pushd dist/build/adnauseam.firefox
 
 #web-ext run -v --no-reload --firefox-profile="${1:-default}"
 #web-ext run -v --bc --no-reload --firefox-profile="${1:-default}" --firefox="${2:-firefox}"
