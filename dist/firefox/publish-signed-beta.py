1364e4836e2775e9f110e4e2f83278bd9d8b2d4d
Raymond Hill <rhill@raymondhill.net>
2019-12-05 09:36:11 -0500

    Fix publishing script for Firefox dev build

--- dist/firefox/publish-signed-beta.py
+++ dist/firefox/publish-signed-beta.py
@@ -226,6 +226,8 @@ with open(unsigned_xpi_filepath, 'rb') as f:
             exit(1)
         if not signing_check_response['files'] or len(signing_check_response['files']) == 0:
             continue
+        if not signing_check_response['files'][0]['signed']:
+            continue
         if not signing_check_response['files'][0]['download_url']:
             print('Error: AMO signing failed')
             print(response.text)
