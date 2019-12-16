382f54ca36aa941696d674cef0e0aebbb6a36266
cqx931 <cqx931@gmail.com>
2019-12-16 17:09:38 -0500

    more fixes for icons

--- tools/make-chromium.sh
+++ tools/make-chromium.sh
@@ -27,7 +27,6 @@ cp -R src/lib               $DES/
 #cp -R $DES/_locales/nb      $DES/_locales/no
 cp src/*.html               $DES/
 cp platform/chromium/*.js   $DES/js/
-cp -R platform/chromium/img $DES/
 cp platform/chromium/*.html $DES/
 cp platform/chromium/*.json $DES/
 cp manifest.json $DES/            # use ADN manifest, not ublock's
