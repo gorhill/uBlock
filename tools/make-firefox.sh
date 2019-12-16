7e9e3fa0bf845c129111dc6d0d3653a38e10d35b
cqx931 <cqx931@gmail.com>
2019-12-16 17:02:12 -0500

    fix firefox imgs

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -24,7 +24,6 @@ cp -R src/lib                    $DES/
 #cp -R $DES/_locales/nb           $DES/_locales/no
 cp src/*.html                    $DES/
 cp platform/chromium/*.js        $DES/js/
-cp -R platform/chromium/img      $DES/
 cp platform/chromium/*.html      $DES/
 cp platform/chromium/*.json      $DES/
 cp LICENSE.txt                   $DES/
