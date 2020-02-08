9d0008886ec835d02ea6faf1120c77b5e9b8bc5e
JustOff <Off.Just.Off@gmail.com>
2020-02-08 16:15:22 +0200

    Add `env_legacy` to the pre-processor supported tokens
    This will allow specifically target uBlock Origin for Firefox legacy-based browsers in shared filter lists,
    see https://github.com/gorhill/uBlock-for-firefox-legacy/pull/1.

--- src/js/storage.js
+++ src/js/storage.js
@@ -957,6 +957,7 @@ self.addEventListener('hiddenSettingsChanged', ( ) => {
     [ 'env_chromium', 'chromium' ],
     [ 'env_edge', 'edge' ],
     [ 'env_firefox', 'firefox' ],
+    [ 'env_legacy', 'legacy' ],
     [ 'env_mobile', 'mobile' ],
     [ 'env_safari', 'safari' ],
     [ 'cap_html_filtering', 'html_filtering' ],
