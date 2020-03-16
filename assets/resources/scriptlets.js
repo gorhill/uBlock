40ea9d69d5d039bf0681cb84729641e33d156bc3
Raymond Hill <rhill@raymondhill.net>
2020-03-16 08:47:34 -0400

    Fix regression in set-constant scriptlet
    Related issue:
    - https://github.com/uBlockOrigin/uBlock-issues/issues/947
    
    `Object.prototype` is not an instance of `Object`, but
    yet is still a JS object; thus the solution is to
    additionally compare against `typeof`.

--- assets/resources/scriptlets.js
+++ assets/resources/scriptlets.js
@@ -549,7 +549,7 @@
         const prop = chain.slice(0, pos);
         let v = owner[prop];
         chain = chain.slice(pos + 1);
-        if ( v instanceof Object ) {
+        if ( v instanceof Object || typeof v === 'object' ) {
             makeProxy(v, chain);
             return;
         }
