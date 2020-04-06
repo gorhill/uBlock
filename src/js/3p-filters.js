a45a21f337216d7d4aaddb5a454ed4842eca3806
Raymond Hill <rhill@raymondhill.net>
2020-04-06 14:00:19 -0400

    Second pass to re-design the dashboard
    Related commit:
    - https://github.com/gorhill/uBlock/commit/453f5450b6e6550618bf97b5b0541f5f46c35d8e
    
    The changes in this commit are mostly related
    to the "Filter lists" pane.
    
    An eye icon has been added, which purpose is
    to view the content of a list. Clicking on the
    name of the list will toggle the list's
    checkbox.

--- src/js/3p-filters.js
+++ src/js/3p-filters.js
@@ -86,15 +86,17 @@ const renderFilterLists = function(soft) {
             li = listEntryTemplate.clone().nodeAt(0);
         }
         const on = entry.off !== true;
+        li.classList.toggle('checked', on);
         let elem;
         if ( li.getAttribute('data-listkey') !== listKey ) {
             li.setAttribute('data-listkey', listKey);
             elem = li.querySelector('input[type="checkbox"]');
             elem.checked = on;
-            elem = li.querySelector('a:nth-of-type(1)');
+            elem = li.querySelector('.listname');
+            elem.textContent = listNameFromListKey(listKey);
+            elem = li.querySelector('a.content');
             elem.setAttribute('href', 'asset-viewer.html?url=' + encodeURI(listKey));
             elem.setAttribute('type', 'text/html');
-            elem.textContent = listNameFromListKey(listKey);
             li.classList.remove('toRemove');
             if ( entry.supportName ) {
                 li.classList.add('support');
@@ -321,7 +323,7 @@ const renderWidgets = function() {
     );
     uDom('#buttonUpdate').toggleClass(
         'disabled',
-        document.querySelector('body:not(.updating) #lists .listEntry.obsolete:not(.toRemove) > input[type="checkbox"]:checked') === null
+        document.querySelector('body:not(.updating) #lists .listEntry.obsolete:not(.toRemove) input[type="checkbox"]:checked') === null
     );
 };
 
@@ -377,20 +379,28 @@ const hashFromCurrentFromSettings = function() {
 
 /******************************************************************************/
 
+const onListsetChanged = function(ev) {
+    const input = ev.target;
+    const li = input.closest('.listEntry');
+    li.classList.toggle('checked', input.checked);
+    onFilteringSettingsChanged();
+};
+
+/******************************************************************************/
+
 const onFilteringSettingsChanged = function() {
     renderWidgets();
 };
 
 /******************************************************************************/
 
-const onRemoveExternalList = function(ev) {
+const onRemoveExternalList = function() {
     const liEntry = uDom(this).ancestors('[data-listkey]');
     const listKey = liEntry.attr('data-listkey');
     if ( listKey ) {
         liEntry.toggleClass('toRemove');
         renderWidgets();
     }
-    ev.preventDefault();
 };
 
 /******************************************************************************/
@@ -539,7 +549,7 @@ const toggleHideUnusedLists = function(which) {
         groupSelector = '.groupEntry[data-groupkey="' + which + '"] ';
         uDom(groupSelector).toggleClass('hideUnused', mustHide);
     }
-    uDom(groupSelector + '.listEntry > input[type="checkbox"]:not(:checked)')
+    uDom(groupSelector + '.listEntry input[type="checkbox"]:not(:checked)')
         .ancestors('.listEntry[data-listkey]')
         .toggleClass('unused', mustHide);
     vAPI.localStorage.setItem(
@@ -549,7 +559,7 @@ const toggleHideUnusedLists = function(which) {
 };
 
 const revealHiddenUsedLists = function() {
-    uDom('#lists .listEntry.unused > input[type="checkbox"]:checked')
+    uDom('#lists .listEntry.unused input[type="checkbox"]:checked')
         .ancestors('.listEntry[data-listkey]')
         .removeClass('unused');
 };
@@ -659,8 +669,8 @@ uDom('#buttonUpdate').on('click', ( ) => { buttonUpdateHandler(); });
 uDom('#buttonPurgeAll').on('click', ev => {
     buttonPurgeAllHandler(ev.ctrlKey && ev.shiftKey);
 });
-uDom('#lists').on('change', '.listEntry > input', onFilteringSettingsChanged);
-uDom('#lists').on('click', '.listEntry > a.remove', onRemoveExternalList);
+uDom('#lists').on('change', '.listEntry input', onListsetChanged);
+uDom('#lists').on('click', '.listEntry .remove', onRemoveExternalList);
 uDom('#lists').on('click', 'span.cache', onPurgeClicked);
 uDom('#externalLists').on('input', onFilteringSettingsChanged);
 
