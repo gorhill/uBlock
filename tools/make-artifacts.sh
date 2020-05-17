d21f7ca9ffcfcc0a791a4eaadfc35b59d2eb3ec3
dhowe <dhowe@cims.nyu.edu>
2020-05-17 16:15:57 +0800

    tweaks

--- tools/make-artifacts.sh
+++ tools/make-artifacts.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+DO_FIREFOX=true
+DO_CHROME=true
+DO_OPERA=true
+DO_EDGE=true
+
 CHROME=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 FIREFOX=/Applications/Firefox.app/Contents/MacOS/firefox-bin
 OPERA=/Applications/Opera.app/Contents/MacOS/Opera
-
-#CHROME_PEM=./adnauseam.chromium.pem
 
 # before running, check for dependencies, if not, exit with error
 hash web-ext 2>/dev/null || { echo >&2 "Webext is not installed. Please do npm install --global web-ext."; exit 1; }
@@ -15,7 +18,6 @@ ARTS=artifacts
 
 CHROME_OPTS=--pack-extension=${DES}/adnauseam.chromium
 OPERA_OPTS=--pack-extension=${DES}/adnauseam.opera
-
 VERSION=`jq .version manifest.json | tr -d '"'`
 
 
@@ -26,43 +28,55 @@ rm -rf ${DES}/*
 
 
 # CHROME (ZIP/CRX)
-printf '%s' "*** Target -> "
-command "${CHROME}" --version || { echo >&2 "Chrome is not installed."; exit 1; }
-./tools/make-chromium.sh
-if [ -f $CHROME_PEM ]; then  # do we have the signing key?
-  "${CHROME}" "${CHROME_OPTS}" "--pack-extension-key $CHROME_PEM" > /dev/null 2>&1
-  mv ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium.crx
-  echo "*** AdNauseam.chromium: Signed with local .pem\\n"
-else
-  "${CHROME}" "${CHROME_OPTS}"
-  mv ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium-UNSIGNED.crx
-  echo "WARN: NO .pem key found for Chrome build\\n"
+if [ $DO_CHROME = true ]
+then
+  printf '%s' "*** Target -> "
+  command "${CHROME}" --version || { echo >&2 "Chrome is not installed."; exit 1; }
+  ./tools/make-chromium.sh
+  if [ -f $CHROME_PEM ]; then  # do we have the signing key?
+    "${CHROME}" "${CHROME_OPTS}" "--pack-extension-key $CHROME_PEM" > /dev/null 2>&1
+    mv ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium.crx
+    echo "*** AdNauseam.chromium: Signed with local .pem\\n"
+  else
+    "${CHROME}" "${CHROME_OPTS}"
+    mv ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium-UNSIGNED.crx
+    echo "WARN: NO .pem key found for Chrome build\\n"
+  fi
+  #cp ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium.crx
+  cd ${DES}
+  zip -9 -r -q --exclude=*.DS_Store* ../../artifacts/adnauseam-${VERSION}.chromium.zip adnauseam.chromium
+  cd -
 fi
-#cp ${DES}/adnauseam.chromium.crx ${ARTS}/adnauseam-${VERSION}.chromium.crx
-cd ${DES}
-zip -9 -r -q --exclude=*.DS_Store* ../../artifacts/adnauseam-${VERSION}.chromium.zip adnauseam.chromium
-cd -
 
 
 # OPERA (CRX)
-printf "*** Target -> Opera " && command "${OPERA}" --version || { echo >&2 "Opera is not installed."; exit 1; }
-./tools/make-opera.sh
-"${OPERA}" "${OPERA_OPTS}"
-mv ${DES}/adnauseam.opera.crx ${ARTS}/adnauseam-${VERSION}.opera.crx
+if [ $DO_OPERA = true ]
+then
+  printf "*** Target -> Opera " && command "${OPERA}" --version || { echo >&2 "Opera is not installed."; exit 1; }
+  ./tools/make-opera.sh
+  "${OPERA}" "${OPERA_OPTS}"
+  mv ${DES}/adnauseam.opera.crx ${ARTS}/adnauseam-${VERSION}.opera.crx
+fi
 
 
 # FIREFOX (ZIP)
-printf '%s' "*** Target -> "
-command "${FIREFOX}" -v || { echo >&2 "Firefox is not installed."; exit 1; }
-./tools/make-firefox.sh all
-web-ext build -s ${DES}/adnauseam.firefox -a ${ARTS}
-mv ${ARTS}/adnauseam-${VERSION}.zip ${ARTS}/adnauseam-${VERSION}.firefox.zip
+if [ $DO_FIREFOX = true ]
+then
+  printf '%s' "*** Target -> "
+  command "${FIREFOX}" -v || { echo >&2 "Firefox is not installed."; exit 1; }
+  ./tools/make-firefox.sh all
+  web-ext build -s ${DES}/adnauseam.firefox -a ${ARTS}
+  mv ${ARTS}/adnauseam-${VERSION}.zip ${ARTS}/adnauseam-${VERSION}.firefox.zip
+fi
 
 # EDGE (ZIP)
-printf '%s' "*** Target -> Edge"
-./tools/make-edge.sh
-web-ext build -s ${DES}/adnauseam.edge -a ${ARTS}
-mv ${ARTS}/adnauseam-${VERSION}.zip ${ARTS}/adnauseam-${VERSION}.edge.zip
+if [ $DO_EDGE = true ]
+then
+  printf '%s' "*** Target -> Edge"
+  ./tools/make-edge.sh
+  web-ext build -s ${DES}/adnauseam.edge -a ${ARTS}
+  mv ${ARTS}/adnauseam-${VERSION}.zip ${ARTS}/adnauseam-${VERSION}.edge.zip
+fi
 
 
 # NO PEMS
