911684e7f6d905202bd0bb3d375744fd6bff8bfc
Raymond Hill <rhill@raymondhill.net>
2020-07-06 09:03:20 -0400

    Replace seemingly abandoned malware lists with urlhaus list
    Related issues:
    - https://github.com/uBlockOrigin/uBlock-issues/issues/1116
    - https://github.com/uBlockOrigin/uBlock-issues/issues/984
    
    It has been found that the two malware lists uBO uses by
    default appear to be essentially no longer maintained.
    
    urlhaus list[1] has been identified by the community as
    being well maintained and as being actually useful as a
    default malware-related list in uBO, since it's being updated
    every day from a database of top domains and specific URLs
    identified as serving malicious content. Additionally, the
    maintainer of urlhaus list has taken steps to increase
    compatibility with uBO[2].
    
    The decision has been to replace the current two malware-
    related lists with urlhaus list, which will be enabled by
    default in uBO -- and this means that list will be part of
    uBO's package from now on.
    
    For those who have the two removed malware lists enabled,
    these will be moved to the custom lists section -- they
    will still be enabled. It is suggested users remove them from
    their selection of lists as their usefulness at this point
    is questionable.
    
    [1] https://gitlab.com/curben/urlhaus-filter
    [2] https://gitlab.com/curben/urlhaus-filter/-/commit/859dfd03c6e9e5af6f1fac29bc9b22d1e0046111

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -17,10 +17,9 @@ cp -R ./assets $DES/
 
 mkdir $DES/thirdparties
 cp -R ../uAssets/thirdparties/easylist-downloads.adblockplus.org $DES/thirdparties/
-cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com         $DES/thirdparties/
 cp -R ../uAssets/thirdparties/pgl.yoyo.org                       $DES/thirdparties/
 cp -R ../uAssets/thirdparties/publicsuffix.org                   $DES/thirdparties/
-cp -R ../uAssets/thirdparties/www.malwaredomainlist.com          $DES/thirdparties/
+cp -R ../uAssets/thirdparties/urlhaus-filter                     $DES/thirdparties/
 
 mkdir $DES/ublock
 cp -R ../uAssets/filters/*                                       $DES/ublock/
