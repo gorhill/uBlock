b02970f5eee014c1cff444462967fc628c910689
Raymond Hill <rhill@raymondhill.net>
2021-07-29 11:46:35 -0400

    Add command to update submodules

---
+++ tools/update-submodules.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# This script assumes a linux environment
+
+echo "*** Update submodules"
+
+git submodule update --remote
+if [ `git diff --quiet ./submodules/` ]; then
+    git add -u submodules/
+    git commit -m 'Update submodules'
+    git push origin master
+    echo "*** Submodules updated"
+else
+    echo "*** Submodules are already up to date"
+fi
