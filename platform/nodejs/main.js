5be4d5d877c64a887961635e95aa0bda47d775ec
Raymond Hill <rhill@raymondhill.net>
2021-07-30 07:00:45 -0400

    Make pslInit() returns the publicSuffixList instance used internally
    Related discussion:
    - https://github.com/uBlockOrigin/uBlock-issues/issues/1664#issuecomment-889647595

--- platform/nodejs/main.js
+++ platform/nodejs/main.js
@@ -123,6 +123,7 @@ function pslInit(raw) {
         }
     }
     globals.publicSuffixList.parse(raw, globals.punycode.toASCII);
+    return globals.publicSuffixList;
 }
 
 function restart(lists, options = {}) {
