de1feb0948f4452b54449eb03a81aad724a39917
Raymond Hill <rhill@raymondhill.net>
2021-07-31 08:59:55 -0400

    Fix make-nodejs
    Broken due to renaming text-iterators to text-utils.

--- tools/make-browser.sh
+++ tools/make-browser.sh
@@ -13,7 +13,7 @@ cp src/js/hntrie.js                  $DES/js
 cp src/js/static-filtering-parser.js $DES/js
 cp src/js/static-net-filtering.js    $DES/js
 cp src/js/static-filtering-io.js     $DES/js
-cp src/js/text-iterators.js          $DES/js
+cp src/js/text-utils.js              $DES/js
 cp src/js/uri-utils.js               $DES/js
 
 mkdir -p $DES/js/wasm
