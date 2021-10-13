b0038eac28457180b4c95364e53312031c09ca6c
Raymond Hill <rhill@raymondhill.net>
2021-10-13 11:17:21 -0400

    Remove unused code path
    Versions of Firefox 54 and lower are no longer supported.

--- platform/common/vapi-background.js
+++ platform/common/vapi-background.js
@@ -410,16 +410,6 @@ vAPI.Tabs = class {
             return;
         }
 
-        // https://github.com/gorhill/uBlock/issues/3053#issuecomment-332276818
-        //   Do not try to lookup uBO's own pages with FF 55 or less.
-        if (
-            vAPI.webextFlavor.soup.has('firefox') &&
-            vAPI.webextFlavor.major < 56
-        ) {
-            this.create(targetURL, details);
-            return;
-        }
-
         // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/query#Parameters
         //   "Note that fragment identifiers are not matched."
         //   Fragment identifiers ARE matched -- we need to remove the fragment.
