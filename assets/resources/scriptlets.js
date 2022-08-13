78fccadaf243937b8b5d51f68c5ef44d04a4770c
Raymond Hill <rhill@raymondhill.net>
2022-08-13 09:09:36 -0400

    Fix bad alias
    Related feedback:
    - https://github.com/DandelionSprout/adfilt/issues/63#issuecomment-1214131551

--- assets/resources/scriptlets.js
+++ assets/resources/scriptlets.js
@@ -1290,7 +1290,7 @@
 
 
 // https://github.com/gorhill/uBlock/issues/1228
-/// window.name-defuser
+/// window.name-defuser.js
 (function() {
     if ( window === window.top ) {
         window.name = '';
