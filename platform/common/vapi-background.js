ec53e52a63b73247f5aa8dde681ed16e20da1f00
Raymond Hill <rhill@raymondhill.net>
2023-04-10 17:10:00 -0400

    Replace toolbar icon tooltip with message section in panel
    Related issue:
    - https://github.com/uBlockOrigin/uBlock-issues/issues/2585
    
    Related feedback:
    - https://www.reddit.com/r/uBlockOrigin/comments/126rn4f/yellow_toolbar_icon_exclamation_mark_badge/jfns0pc/

--- platform/common/vapi-background.js
+++ platform/common/vapi-background.js
@@ -677,7 +677,6 @@ if ( webext.browserAction instanceof Object ) {
 {
     const browserAction = vAPI.browserAction;
     const titleTemplate = `${browser.runtime.getManifest().browser_action.default_title} ({badge})`;
-    const browserLaunchIssue = browser.i18n.getMessage('unprocessedRequestTooltip');
     const icons = [
         { path: { '16': 'img/icon_16-off.png', '32': 'img/icon_32-off.png' } },
         { path: { '16':     'img/icon_16.png', '32':     'img/icon_32.png' } },
@@ -804,10 +803,9 @@ if ( webext.browserAction instanceof Object ) {
         // - the platform does not support browserAction.setIcon(); OR
         // - the rendering of the badge is disabled
         if ( browserAction.setTitle !== undefined ) {
-            const title = hasUnprocessedRequest && browserLaunchIssue ||
-                titleTemplate.replace('{badge}',
-                    state === 1 ? (badge !== '' ? badge : '0') : 'off'
-                );
+            const title = titleTemplate.replace('{badge}',
+                state === 1 ? (badge !== '' ? badge : '0') : 'off'
+            );
             browserAction.setTitle({ tabId: tab.id, title });
         }
 
