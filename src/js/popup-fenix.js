gorhill/uBlock

commit 0d77ccded7bd35adf5161d11071c37f382675073

Add advanced setting to force popup panel orientation
Related discussion:
https://github.com/uBlockOrigin/uBlock-issues/discussions/2419#discussioncomment-8746679

Name: `popupPanelOrientation`

Supported values:
- `unset` (default): uBO decides whichever mode is best
- `landscape`: force popup panel to landscape mode
- `portrait`: force popup panel to portrait mode

src/js/popup-fenix.js
@@ -802,7 +802,7 @@ let renderOnce = function() {
         dom.attr('#firewall [title][data-src]', 'title', null);
     }
 
-    // This must be done the firewall is populated
+    // This must be done when the firewall is populated
     if ( popupData.popupPanelHeightMode === 1 ) {
         dom.cl.add(dom.body, 'vMin');
     }
@@ -1462,6 +1462,31 @@ const getPopupData = async function(tabId, first = false) {
         }
     };
 
+    const setOrientation = async ( ) => {
+        if ( dom.cl.has(dom.root, 'mobile') ) {
+            dom.cl.remove(dom.root, 'desktop');
+            dom.cl.add(dom.root, 'portrait');
+            return;
+        }
+        if ( selfURL.searchParams.get('portrait') !== null ) {
+            dom.cl.add(dom.root, 'portrait');
+            return;
+        }
+        if ( popupData.popupPanelOrientation === 'landscape' ) { return; }
+        if ( popupData.popupPanelOrientation === 'portrait' ) {
+            dom.cl.add(dom.root, 'portrait');
+            return;
+        }
+        if ( dom.cl.has(dom.root, 'desktop') === false ) { return; }
+        await nextFrames(8);
+        const main = qs$('#main');
+        const firewall = qs$('#firewall');
+        const minWidth = (main.offsetWidth + firewall.offsetWidth) / 1.1;
+        if ( window.innerWidth < minWidth ) {
+            dom.cl.add(dom.root, 'portrait');
+        }
+    };
+
     // The purpose of the following code is to reset to a vertical layout
     // should the viewport not be enough wide to accommodate the horizontal
     // layout.
@@ -1474,24 +1499,7 @@ const getPopupData = async function(tabId, first = false) {
     // Use a tolerance proportional to the sum of the width of the panes
     // when testing against viewport width.
     const checkViewport = async function() {
-        if (
-            dom.cl.has(dom.root, 'mobile') ||
-            selfURL.searchParams.get('portrait')
-        ) {
-            dom.cl.add(dom.root, 'portrait');
-            dom.cl.remove(dom.root, 'desktop');
-        } else if ( dom.cl.has(dom.root, 'desktop') ) {
-            await nextFrames(8);
-            const main = qs$('#main');
-            const firewall = qs$('#firewall');
-            const minWidth = (main.offsetWidth + firewall.offsetWidth) / 1.1;
-            if (
-                selfURL.searchParams.get('portrait') ||
-                window.innerWidth < minWidth
-            ) {
-                dom.cl.add(dom.root, 'portrait');
-            }
-        }
+        await setOrientation();
         if ( dom.cl.has(dom.root, 'portrait') ) {
             const panes = qs$('#panes');
             const sticky = qs$('#sticky');

src/js/background.js
@@ -80,8 +80,9 @@ const hiddenSettingsDefault = {
     modifyWebextFlavor: 'unset',
     popupFontSize: 'unset',
     popupPanelDisabledSections: 0,
-    popupPanelLockedSections: 0,
     popupPanelHeightMode: 0,
+    popupPanelLockedSections: 0,
+    popupPanelOrientation: 'unset',
     requestJournalProcessPeriod: 1000,
     requestStatsDisabled: false,
     selfieDelayInSeconds: 53,

src/js/messaging.js
@@ -378,6 +378,7 @@ const popupDataFromTabId = function(tabId, tabTitle) {
         popupPanelDisabledSections: µbhs.popupPanelDisabledSections,
         popupPanelLockedSections: µbhs.popupPanelLockedSections,
         popupPanelHeightMode: µbhs.popupPanelHeightMode,
+        popupPanelOrientation: µbhs.popupPanelOrientation,
         tabId,
         tabTitle,
         tooltipsDisabled: µbus.tooltipsDisabled,

commit 94de4e266e49b458cb3c6e1e67be755cbd63ea5c

Fix popup panel when embedded in logger
Regression from commit:
https://github.com/gorhill/uBlock/commit/0d77ccded7

Related feedback:
https://github.com/uBlockOrigin/uBlock-issues/discussions/2419#discussioncomment-8759449

src/js/popup-fenix.js
@@ -70,6 +70,9 @@ let cachedPopupHash = '';
 const reCyrillicNonAmbiguous = /[\u0400-\u042b\u042d-\u042f\u0431\u0432\u0434\u0436-\u043d\u0442\u0444\u0446-\u0449\u044b-\u0454\u0457\u0459-\u0460\u0462-\u0474\u0476-\u04ba\u04bc\u04be-\u04ce\u04d0-\u0500\u0502-\u051a\u051c\u051e-\u052f]/;
 const reCyrillicAmbiguous = /[\u042c\u0430\u0433\u0435\u043e\u043f\u0440\u0441\u0443\u0445\u044a\u0455\u0456\u0458\u0461\u0475\u04bb\u04bd\u04cf\u0501\u051b\u051d]/;
 
+const hasOwnProperty = (o, p) =>
+    Object.prototype.hasOwnProperty.call(o, p);
+
 /******************************************************************************/
 
 const cachePopupData = function(data) {
@@ -88,7 +91,7 @@ const cachePopupData = function(data) {
         return popupData;
     }
     for ( const hostname in hostnameDict ) {
-        if ( hostnameDict.hasOwnProperty(hostname) === false ) { continue; }
+        if ( hasOwnProperty(hostnameDict, hostname) === false ) { continue; }
         let domain = hostnameDict[hostname].domain;
         let prefix = hostname.slice(0, 0 - domain.length - 1);
         // Prefix with space char for 1st-party hostnames: this ensure these
@@ -160,7 +163,7 @@ const formatNumber = function(count) {
         });
         if (
             intl.resolvedOptions instanceof Function &&
-            intl.resolvedOptions().hasOwnProperty('notation')
+            hasOwnProperty(intl.resolvedOptions(), 'notation')
         ) {
             intlNumberFormat = intl;
         }
@@ -545,7 +548,7 @@ const renderPrivacyExposure = function() {
         if ( des === '*' || desHostnameDone.has(des) ) { continue; }
         const hnDetails = hostnameDict[des];
         const { domain, counts } = hnDetails;
-        if ( allDomains.hasOwnProperty(domain) === false ) {
+        if ( hasOwnProperty(allDomains, domain) === false ) {
             allDomains[domain] = false;
             allDomainCount += 1;
         }
@@ -1469,11 +1472,13 @@ const getPopupData = async function(tabId, first = false) {
             return;
         }
         if ( selfURL.searchParams.get('portrait') !== null ) {
+            dom.cl.remove(dom.root, 'desktop');
             dom.cl.add(dom.root, 'portrait');
             return;
         }
         if ( popupData.popupPanelOrientation === 'landscape' ) { return; }
         if ( popupData.popupPanelOrientation === 'portrait' ) {
+            dom.cl.remove(dom.root, 'desktop');
             dom.cl.add(dom.root, 'portrait');
             return;
         }