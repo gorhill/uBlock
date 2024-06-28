57ed2937606fe6c9876efb218724d01dc584498f
Raymond Hill <rhill@raymondhill.net>
2024-06-28 13:39:38 -0400

    [mv3] Fix bad test re. managed storage

--- platform/mv3/extension/js/ext.js
+++ platform/mv3/extension/js/ext.js
@@ -107,7 +107,7 @@ export async function sessionWrite(key, value) {
 
 export async function adminRead(key) {
     if ( browser.storage instanceof Object === false ) { return; }
-    if ( browser.storage.local instanceof Object === false ) { return; }
+    if ( browser.storage.managed instanceof Object === false ) { return; }
     try {
         const bin = await browser.storage.managed.get(key);
         if ( bin instanceof Object === false ) { return; }
