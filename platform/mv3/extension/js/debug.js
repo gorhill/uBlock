e73eb23c9047ec79945a27b7ac3810097485d525
Raymond Hill <rhill@raymondhill.net>
2024-08-17 10:42:17 -0400

    [mv3] Don't filter out tabless requests in "logger"

--- platform/mv3/extension/js/debug.js
+++ platform/mv3/extension/js/debug.js
@@ -112,7 +112,9 @@ export const getMatchedRules = (( ) => {
             const j = (writePtr + i) % bufferSize;
             const ruleInfo = matchedRules[j];
             if ( ruleInfo === null ) { continue; }
-            if ( ruleInfo.request.tabId !== tabId ) { continue; }
+            if ( ruleInfo.request.tabId !== -1 ) {
+                if ( ruleInfo.request.tabId !== tabId ) { continue; }
+            }
             const promise = getRuleDetails(ruleInfo);
             if ( promise === undefined ) { continue; }
             promises.unshift(promise);
