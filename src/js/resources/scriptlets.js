9e946ce0c3cbbaf9c147c3d5e023f8d376a92632
Raymond Hill <rhill@raymondhill.net>
2025-03-15 07:50:28 -0400

    Improve `trusted-override-element-method` scriptlet
    As discussed with filter list maintainers:
    https://github.com/uBlockOrigin/uBlock-discussions/discussions/933

--- src/js/resources/scriptlets.js
+++ src/js/resources/scriptlets.js
@@ -3472,6 +3472,7 @@ function trustedOverrideElementMethod(
     if ( methodPath === '' ) { return; }
     const safe = safeSelf();
     const logPrefix = safe.makeLogPrefix('trusted-override-element-method', methodPath, selector, disposition);
+    const extraArgs = safe.getExtraArgs(Array.from(arguments), 3);
     proxyApplyFn(methodPath, function(context) {
         let override = selector === '';
         if ( override === false ) {
@@ -3492,7 +3493,7 @@ function trustedOverrideElementMethod(
         if ( disposition === 'throw' ) {
             throw new ReferenceError();
         }
-        return validateConstantFn(false, disposition);
+        return validateConstantFn(true, disposition, extraArgs);
     });
 }
 
