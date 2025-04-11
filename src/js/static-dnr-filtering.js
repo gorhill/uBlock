11e159fd317267c3b8eba9309d1d80239791b5e0
Raymond Hill <rhill@raymondhill.net>
2025-04-11 09:49:03 -0400

    [mv3] Discard regex- or path-based targets in static extended filters
    There is no support yet for such filters in uBO Lite.

--- src/js/static-dnr-filtering.js
+++ src/js/static-dnr-filtering.js
@@ -31,7 +31,7 @@ import staticNetFilteringEngine from './static-net-filtering.js';
 
 /******************************************************************************/
 
-const isRegex = hn => hn.startsWith('/') && hn.endsWith('/');
+const isRegexOrPath = hn => hn.includes('/');
 
 /******************************************************************************/
 
@@ -112,7 +112,7 @@ function addExtendedToDNR(context, parser) {
         for ( const { hn, not, bad } of parser.getExtFilterDomainIterator() ) {
             if ( bad ) { continue; }
             if ( exception ) { continue; }
-            if ( isRegex(hn) ) { continue; }
+            if ( isRegexOrPath(hn) ) { continue; }
             let details = context.scriptletFilters.get(argsToken);
             if ( details === undefined ) {
                 context.scriptletFilters.set(argsToken, details = { args });
@@ -170,7 +170,7 @@ function addExtendedToDNR(context, parser) {
         };
         for ( const { hn, not, bad } of parser.getExtFilterDomainIterator() ) {
             if ( bad ) { continue; }
-            if ( isRegex(hn) ) { continue; }
+            if ( isRegexOrPath(hn) ) { continue; }
             if ( not ) {
                 if ( rule.condition.excludedInitiatorDomains === undefined ) {
                     rule.condition.excludedInitiatorDomains = [];
@@ -225,7 +225,7 @@ function addExtendedToDNR(context, parser) {
     for ( const { hn, not, bad } of parser.getExtFilterDomainIterator() ) {
         if ( bad ) { continue; }
         if ( not && exception ) { continue; }
-        if ( isRegex(hn) ) { continue; }
+        if ( isRegexOrPath(hn) ) { continue; }
         if ( details === undefined ) {
             context.specificCosmeticFilters.set(compiled, details = {});
         }
