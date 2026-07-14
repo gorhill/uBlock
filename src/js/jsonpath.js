7dfeb93a1bebcb5e3b406496ea96a3f68d46dfc5
Raymond Hill <gorhill@users.noreply.github.com>
2026-07-14 15:48:26 -0400

    [jsonpath] Fix regression when compiling unquoted identifier
    Related feedback:
    https://github.com/uBlockOrigin/uBlock-issues/issues/4047#issuecomment-4948900203

--- src/js/jsonpath.js
+++ src/js/jsonpath.js
@@ -101,8 +101,8 @@ export class JSONPath {
     }
     compile(query) {
         this.#compiled = undefined;
-        const v2 = query.startsWith('v2:');
-        if ( v2 ) { query = query.slice(3); }
+        this.v2 = query.startsWith('v2:');
+        if ( this.v2 ) { query = query.slice(3); }
         const r = this.#compile(query, 0);
         if ( r === undefined ) { return; }
         if ( r.i !== query.length ) {
@@ -122,7 +122,7 @@ export class JSONPath {
             try { r.rval = JSON.parse(val); }
             catch { return; }
         }
-        r.v2 = v2;
+        r.v2 = this.v2;
         this.#compiled = r;
     }
     evaluate(root) {
@@ -468,7 +468,7 @@ export class JSONPath {
                 needIdentifier = false;
                 continue;
             }
-            if ( this.#compiled.v2 ) { return; }
+            if ( this.v2 ) { return; }
             const r = this.#consumeUnquotedIdentifier(query, i);
             if ( r === undefined ) { return; }
             keys.push(r.s);
